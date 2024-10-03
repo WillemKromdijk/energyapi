ffa583f820209a291105234a0a84f3707317eb39
Willem Kromdijk <49718297+WillemKromdijk@users.noreply.github.com>
2024-10-03 14:30:40 +0200

    Update S3Service.java

--- adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
+++ adminpanel/app/src/main/java/com/energy/admin/service/S3Service.java
@@ -16,8 +16,8 @@ public class S3Service {
     private final S3Client s3Client;
 
     public S3Service() {
-        String aws_access_key_id = "AKIA<placeholder>";
-        String aws_secret_access_key = "<placeholder>";
+        String aws_access_key_id = "AKIA2UC3BSXMDVY76F4X";
+        String aws_secret_access_key = "F7tv9D4SwM0QnvYPEMCKqzPhiV1145whjHK+C+pP";
  
         AwsBasicCredentials awsCredentials = AwsBasicCredentials.create(aws_access_key_id, aws_secret_access_key);
 
